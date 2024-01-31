78307c83b00e58d06c9699da13324da9f6d1fdc1
adityasoam55 <ADITYASOAM55@GMAIL.COM>
2024-01-31 12:46:30 +0530

    Navbar updated

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { IoBagOutline } from "react-icons/io5";
+import { Link } from 'react-router-dom';
+
 
 function NavBar({ productCount }) {
     return (
         <div className='flex gap-5 justify-end w-full bg-black items-center px-4 py-2 sticky'>
             <nav nav className='text-white flex items-center gap-4' >
-                <a className='px-3 font-bold' href=''>Home</a>
+            <Link to="/" className='px-3 font-bold'>Home</Link>
                 <a className='px-3 font-bold' href=''>Category</a>
                 <a className='px-3 font-bold' href=''>Offers</a>
                 <a className='px-3 font-bold' href=''>Account</a>
